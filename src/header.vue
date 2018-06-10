43b31d6757e70088fa1bceed0c4cf93dbf2ed7db
Aldo Sorgentini <aldo.sorgentini@gmail.com>
2018-06-10 17:45:05 -0300

    agregue el componente header, pero no logre hacerlo andar

---
+++ src/header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container-flex header-vacio">
+        <div>Holamanola.</div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        name: 'header',
+        data: function() {
+            return {
+                buscador: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .header-vacio {
+        background: #003399;
+        height: 30vh;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import header from './header.vue'
 
 new Vue({
   el: '#app',
